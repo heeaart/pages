2bbfc9163b76f1c171253116eaab3cebb9bc3a61
aliha <ali.hatbi@wp.pl>
2023-01-29 16:03:53 +0100

    Public

---
+++ assets/js/desktop/desktop_content_loader.js
@@ -0,0 +1,6 @@
+var pq = new PandaRequest(5);
+pq.loadContent(q('#about'), 'content/about.html');
+pq.loadContent(q('#skills'), 'content/skills.html');
+pq.loadContent(q('#projects'), 'content/projects.html');
+pq.loadContent(q('#music'), 'content/music.html');
+pq.loadContent(q('#contact'), 'content/contact.html');
---
+++ assets/js/mobile/desktop_checker.js
@@ -0,0 +1,20 @@
+var detector = new MobileDetect(window.navigator.userAgent);
+var red = false;
+
+if (!(detector.mobile() != null && detector.userAgent() == "Firefox")) {
+    check();
+    setInterval(check, 250);
+}
+
+function check() {
+    if (red) {
+        return;
+    }
+
+    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+    if (width > 800) {
+        red = true;
+        window.location = "https://heeaart.eu/";
+    }
+}
